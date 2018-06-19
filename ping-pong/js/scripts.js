f933058091417770931935c3f5955965934a568a
Student <example@example.com>
2018-06-18 20:46:56 -0700

    removed a few edits and added in emptyArray to display messages for 10 and number

--- ping-pong/js/scripts.js
+++ ping-pong/js/scripts.js
@@ -1,9 +1,16 @@
 // BizLogic
-var enteredNumber= function(number) {
-  if (number % 5 === 0){
+var emptyArray = []
+
+var enteredNumber = function(number) {
+  if (number % 10 === 0){
+    emptyArray.push("I'm a ten!");
+    $(".result").text(emptyArray);
+  } else if (number % 5 === 0){
     return true;
-  } else {
-    return false;
+  }
+  else {
+    emptyArray.push(number);
+    $(".result").text(emptyArray);
   }
 };
 
@@ -13,7 +20,6 @@ $(document).ready(function() {
   $("form#pingpong").submit(function(event){
     event.preventDefault();
     var number = parseInt($("input#pingorpong").val());
-    var result = enteredNumber(number)
-    $(".result").text(result);
+    enteredNumber(number);
   });
 });
