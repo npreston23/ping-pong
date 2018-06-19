0e9dd420694c410c71a73ac7e713979dfe7ce187
Student <example@example.com>
2018-06-18 20:13:01 -0700

    updated text box to a form box. added in a js skeleton

---
+++ ping-pong/js/scripts.js
@@ -0,0 +1,14 @@
+// BizLogic
+$(document).ready(function(){
+  if()
+});
+
+
+// UI Logic
+$(document).ready(function() {
+  $("numbers").submit(function(event){
+    event.preventDefault();
+
+    var return = function(number)
+  }
+});
